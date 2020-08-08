95a656bc0b1e62101649239c250989909aabe777
agustin <agustin@3bytes.com>
2020-08-08 03:21:21 -0300

    getting started with project added navbar in the home page with vuetify.

---
+++ src/views/home/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-app-bar app color="white">
+      <v-toolbar-title>CleanHouse</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn class="ma-2" rounded elevation outlined>
+        <v-icon left>mdi-magnify</v-icon>Buscar
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn small elevation color>Iniciar Sesion</v-btn>
+    </v-app-bar>
+    <div>
+      <v-main>
+        <v-container fluid class="fill-height">
+          <v-row>
+            <v-col>
+              <!-- DESCRIPCITION SECTION -->
+            </v-col>
+            <v-col>
+              <!-- TODO:"REGISTER FORM COMPONENT" -->
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
